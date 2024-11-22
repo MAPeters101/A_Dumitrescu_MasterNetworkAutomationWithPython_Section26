1e78ef2f8d9765c112296fd9473e42c9f265e4ba
Mark Peters <mapeters101@earthlink.net>
2024-11-22 16:19:09 -0500

    Lesson233:  Work on lesson to 5:20.

---
+++ lesson233.py
@@ -0,0 +1,17 @@
+
+def difference(a, b):
+    result = a - b
+    return result
+
+#print(difference())
+#print(difference(1))
+#print(difference(1, 5, 6))
+print(difference(1, 5))
+
+
+def func1(x, y):
+    print(f'1st parameter x is {x}')
+    print(f'2nd parameter y is {y}')
+
+func1('Python', 55)
+func1(55, 'Python')
