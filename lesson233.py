2326b03d0bdfd5858b2bdcb7052871c56f1a51e2
Mark Peters <mapeters101@earthlink.net>
2024-11-22 16:23:58 -0500

    Lesson233:  Complete work on lesson from 5:20.

--- lesson233.py
+++ lesson233.py
@@ -15,3 +15,23 @@ def func1(x, y):
 
 func1('Python', 55)
 func1(55, 'Python')
+print()
+
+
+def func2(x, y, z):
+    print(f'1st parameter x is {x}')
+    print(f'2nd parameter y is {y}')
+    print(f'3rd parameter z is {z}')
+
+func2(y=7, x=3, z=9)
+print()
+func2(10, 20, 30)
+print()
+func2(6, z=1, y=9)
+print()
+#func2(x=6, 1, z=9)
+func2(x=6, y=1, z=9)
+
+
+
+
