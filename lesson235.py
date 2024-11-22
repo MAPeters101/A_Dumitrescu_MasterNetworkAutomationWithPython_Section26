da2771623538b1fd3769ec33e980c40d13e818a0
Mark Peters <mapeters101@earthlink.net>
2024-11-22 16:41:39 -0500

    Lesson235:  Work on lesson to 4:15.

---
+++ lesson235.py
@@ -0,0 +1,27 @@
+
+n = len([1, 2, 4, 5])
+print(n)
+
+def add1(a, b):
+    print(f'Sum: {a + b}')
+
+def add2(a, b):
+    s = a + b
+    return s
+
+def func1():
+    pass
+
+
+add1(10, 20)
+my_sum = add2(5, 2)
+print(my_sum)
+
+x = func1()
+print(x)
+print()
+
+print(add1(1,2))
+
+
+
